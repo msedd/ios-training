c7bcfcc9d7ca358e3e168744a46486a286b3c0d2
Marko Seifert <marko.seifert@t-systems.com>
2015-10-29 13:38:52 +0100

    15.1. Test für Card.scheduleDate

--- Flashcard/Card.swift
+++ Flashcard/Card.swift
@@ -8,9 +8,14 @@
 
 import Foundation
 import CoreData
+import NSDate_Extensions_iOS7
 
 class Card : NSManagedObject{
 
     static let EntityName = "Card"
+    
+    func answeredCorrect(correct : Bool, date : NSDate) {
+        self.scheduleDate = correct ? date.dateByAddingDays(1) : date.dateByAddingHours(1)
+    }
     
 }
--- FlashcardTests/FlashcardTests.swift
+++ FlashcardTests/FlashcardTests.swift
@@ -11,9 +11,16 @@ import XCTest
 
 class FlashcardTests: XCTestCase {
     
+    var db : CoreData!
+    var model : FlashcardsModel!
+    var deck : Deck!
+    var card : Card!
+    
     override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        db = try! CoreData(inMemorySchemaName:"Flashcards")
+        model = FlashcardsModel(managedObjectContext:db.createManagedObjectContext())
+        deck = model.createEnglishDeck()
+        card = deck.cards!.first
     }
     
     override func tearDown() {
@@ -21,16 +28,15 @@ class FlashcardTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testIncorrectScheduled1HourLater() {
+        card.answeredCorrect(false, date:dateFromString("2015-01-05 15:00"))
+        XCTAssertEqual(card.scheduleDate!, dateFromString("2015-01-05 16:00"), "scheduleDate +1h")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+    func dateFromString(str : String) -> NSDate {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+        return dateFormatter.dateFromString(str)!
     }
     
 }
